4cdea059a38c454424d77a862892a3bb54d41aba
upraisertech <akoredesalaudeen54@gmail.com>
2023-10-30 09:01:31 -1100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // colors: {
+      //   "locked-overlay":"linear-gradient(180deg, rgba(255, 255, 255, 0.00) 2.60%, #FFF 37.50%)",
+      //   primary: "#F3BA2F",
+      //   releasesbg: "rgba(235, 250, 250, 1)",
+      //   rankbg: "linear-gradient(180deg, #F8E9DB 0%, #EBFAFA 100%)",
+      //   transparentborder: "rgba(11, 70, 84, 0.25)",
+      //   orangebtn: "#DC9249",
+      //   password: "rgba(208, 201, 214, 1)",
+      //   checkboxborder: "rgba(11, 70, 84, 0.24)",
+      //   inputborder: "rgba(232, 233, 232, 1)",
+      //   cindigo: "rgba(11, 70, 84, 1)",
+
+      // },
+    },
   },
   plugins: [],
 }
